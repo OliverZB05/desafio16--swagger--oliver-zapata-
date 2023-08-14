817a769323236702cda99d99b7eaccfa482880f6
OliverZB05 <oliverzapata.arg@gmail.com>
2023-08-14 17:14:54 -0300

    Delete service directory

--- service/logs.service.js
+++
@@ -1,8 +0,0 @@
-const LogTests = async (req, res, levels) => {
-    levels.forEach(level => {
-        req.logger(req, level, `mensaje de ${level}`);
-    });
-    res.send('Prueba de logs completada');
-}
-
-export { LogTests };
--- service/ticket.service.js
+++
@@ -1,9 +0,0 @@
-import ticketModel from "../dao/dbManagers/models/ticket.model.js";
-
-export default class TicketService {
-    async createTicket(ticketData) {
-        // Crear un nuevo ticket en la base de datos
-        const ticket = await ticketModel.create(ticketData);
-        return ticket;
-    }
-}
