b419a1166fe2e5d584e3edd0a1e3b18efd33c094
OliverZB05 <oliverzapata.arg@gmail.com>
2023-08-14 17:14:16 -0300

    Delete routes directory

--- routes/api/users.router.js
+++
@@ -1,9 +0,0 @@
-// users.router.js
-import { Router } from 'express';
-import passport from 'passport';
-import { userPremium } from '../../service/users.service.js';
-
-const router = Router();
-router.get('/premium/:id', userPremium);
-
-export default router;
